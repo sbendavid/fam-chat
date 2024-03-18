043ca4998e0673b50bf3a8d8e182d882c822fa93
sbendavid <samuelbendavid01@gmail.com>
2024-03-18 10:57:53 +0100

    Little update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+// Middleware
+app.use(express.json());
 
-const port = process.env.PORT  || 3001;
+// Routes
+const indexRoute = require('./routes/index');
+app.use('/', indexRoute);
+
+// Start server
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on ${port}...`));
